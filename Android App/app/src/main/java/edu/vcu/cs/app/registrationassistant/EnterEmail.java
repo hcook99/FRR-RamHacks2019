31d1820ee37d336c1a6906c5371d7bce3229b671
rsderolf <derolfrs@vcu.edu>
2019-09-29 08:04:00 -0400

    DayView Screen Added
    DayView Screen Added

--- Android App/app/src/main/java/edu/vcu/cs/app/registrationassistant/EnterEmail.java
+++ Android App/app/src/main/java/edu/vcu/cs/app/registrationassistant/EnterEmail.java
@@ -1,8 +1,14 @@
 package edu.vcu.cs.app.registrationassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View.OnClickListener;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class EnterEmail extends AppCompatActivity {
 
@@ -10,5 +16,20 @@ public class EnterEmail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_email);
+
+
+        ImageButton buttonUnlock = (ImageButton) findViewById(R.id.loginButton);
+        buttonUnlock.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            //on click of buttonUnlock event- an intent
+            public void onClick(View view) {
+                //second thing is what youre making,
+                Intent unlockIntent = new Intent(getApplicationContext(), DayView.class);
+                startActivity(unlockIntent);
+
+            }
+        });
+
     }
 }
